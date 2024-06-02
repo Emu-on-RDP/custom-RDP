df609af126f4ed46bc99ec0ceee424bb0271e5f7
Emu-on-RDP <abcfack.bd123@gmail.com>
2024-06-02 10:12:47 +0000

    run.py

--- run.py
+++ run.py
@@ -55,7 +55,7 @@ class CRDSetup:
 
     @staticmethod
     def changewall():
-        os.system(f"curl -s -L -k -o Wallpaper.png https://solvecapturetoviewlivematch.blogspot.com/p/wallpaper.html")
+        os.system(f"curl -s -L -k -o Wallpaper.png https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh6S-m4OLmvtcZS8ZTVV6URDle3ElT3wy4mi2Zp1CPZ4B5k8JBHsxfQHoc6X9H0TNI0Mx-vS-Nvedg3Eu9lDkz7vZ0X3vo0xO1OjHLzmVkKMviP52PjB1fkveE51FmfgP-QQv0JGXM-ZnFJMTGU-gfFCQbbQ9DVchPVJ_xUMR5Dx-5hffAxAoCioOST9_0/s892/Wallpaper.png")
         current_directory = os.getcwd()
         custom_wallpaper_path = os.path.join(current_directory, "Wallpaper.png")
         destination_path = '/usr/share/backgrounds/xfce/'
